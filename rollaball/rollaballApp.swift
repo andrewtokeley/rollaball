51dcff83f3f15f9c648fa834c8593475df35d9d2
Andrew Tokeley <andrewtokeley@gmail.com>
2023-02-13 10:26:37 +1300

    Initial Commit

---
+++ rollaball/rollaballApp.swift
@@ -0,0 +1,17 @@
+//
+//  rollaballApp.swift
+//  rollaball
+//
+//  Created by Andrew Tokeley on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct rollaballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
